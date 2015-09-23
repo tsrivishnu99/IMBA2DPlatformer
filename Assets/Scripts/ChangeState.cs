d65e4cc8a88550a76473f220ad51e39d54a279a6
unknown <mj9617@IGMGRAD-12.main.ad.rit.edu>
2015-09-23 14:23:30 -0400

    Different states with key presses.

---
+++ Assets/Scripts/ChangeState.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeState : MonoBehaviour {
+
+    private SphereCollider playerCollider;
+    public PhysicMaterial stickyMaterial;
+    public PhysicMaterial bouncyMaterial;
+    public PhysicMaterial defaultMaterial;
+    public PhysicMaterial heavyMaterial;
+    public float flight_acceleration;
+    private bool flight;
+
+	// Use this for initialization
+	void Start () {
+        flight = false;
+	playerCollider = this.GetComponent<SphereCollider>();
+    setDefault();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Z))
+            sticky();
+
+        if (Input.GetKeyDown(KeyCode.X))
+            bouncy();
+
+        if (Input.GetKeyDown(KeyCode.C))
+            heavy();
+
+        if (Input.GetKeyDown(KeyCode.V))
+            setFlight();
+
+        if(flight)
+        {
+            if (Input.GetButton("Jump"))
+            {
+                this.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0.0f, flight_acceleration, 0.0f), ForceMode.Force);
+            }
+        }
+        }
+
+    void sticky()
+    {
+        setDefault();
+        playerCollider.material = stickyMaterial;
+        this.GetComponent<playerController_v2>().jumpHeight = 1.0f;
+        
+    }
+
+    void bouncy()
+    {
+        setDefault();
+        playerCollider.material = bouncyMaterial;
+    }
+
+    void setDefault()
+    {
+        playerCollider.material = defaultMaterial;
+        this.GetComponent<playerController_v2>().jumpHeight = 2.0f;
+        this.GetComponent<Rigidbody>().mass = 10.0f;
+        this.GetComponent<playerController_v2>().gravity = 10.0f;
+        this.GetComponent<playerController_v2>().speed = 10.0f;
+        this.GetComponent<playerController_v2>().boost = 0.1f;
+        this.GetComponent<playerController_v2>().maxVelocityChange = 5.0f;
+        flight = false;
+    }
+
+    void heavy()
+    {
+        setDefault();
+        playerCollider.material = heavyMaterial;
+        this.GetComponent<playerController_v2>().jumpHeight = 1.0f;
+        this.GetComponent<Rigidbody>().mass = 20.0f;
+    }
+
+    void setFlight()
+    {
+        setDefault();
+        this.GetComponent<playerController_v2>().gravity = 1.0f;
+        this.GetComponent<playerController_v2>().speed = 0.1f;
+        this.GetComponent<playerController_v2>().boost = 0.05f;
+        this.GetComponent<playerController_v2>().maxVelocityChange = 1.0f;
+        flight = true;
+    }
+}
--- Assets/Scripts/playerController_v2.cs
+++ Assets/Scripts/playerController_v2.cs
@@ -8,17 +8,15 @@ public class playerController_v2 : MonoBehaviour
 {
     public float speed = 10.0f;
     public float gravity = 10.0f;
-    public float maxVelocityChange = 10.0f;
+    public float maxVelocityChange = 5.0f;
     public float boost = 0.1f;
     public bool canJump = true;
     public float jumpHeight = 2.0f;
     public float rotationSpeed = 2.0f;
     public bool grounded = false;
 
-	public float bounceFactor;
-
     private Rigidbody rigidbody;
-    
+
     void Awake()
     {
         rigidbody = this.gameObject.GetComponent<Rigidbody>();
@@ -30,7 +28,6 @@ public class playerController_v2 : MonoBehaviour
     {
         if (grounded)
         {
-            // Debug.Log(Input.GetAxis("Horizontal"));
             // Calculate how fast we should be moving
             Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, 0);            // Input.GetAxis("Vertical"));
             targetVelocity = transform.TransformDirection(targetVelocity);
@@ -40,7 +37,7 @@ public class playerController_v2 : MonoBehaviour
             Vector3 velocity = rigidbody.velocity;
             Vector3 velocityChange = (targetVelocity - velocity);
             velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
-            velocityChange.z = 0;   // Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+            velocityChange.z = 0;   
             velocityChange.y = 0;
             rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
 
@@ -52,7 +49,7 @@ public class playerController_v2 : MonoBehaviour
         }
         else
         {
-            if(this.gameObject.GetComponent<player_InputHandler_v3>().leftClamped)
+            //if(this.gameObject.GetComponent<player_InputHandler_v3>().leftClamped)
             {
                 Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, 0);            // Input.GetAxis("Vertical"));
                 targetVelocity = transform.TransformDirection(targetVelocity);
@@ -66,16 +63,6 @@ public class playerController_v2 : MonoBehaviour
         grounded = false;
     }
 
-
-	void OnCollisionEnter(Collision collisionInf)
-	{	
-		//Debug.Log ("collisionEntered");
-		ContactPoint contact = collisionInf.contacts[0];
-
-		rigidbody.velocity = new Vector3(0, CalculateJumpVerticalSpeed(), 0);
-	}
-
-
     void OnCollisionStay(Collision collisionInf)
     {
         if(collisionInf.collider.name.Contains("Platform"))
@@ -86,6 +73,6 @@ public class playerController_v2 : MonoBehaviour
     {
         // From the jump height and gravity we deduce the upwards speed 
         // for the character to reach at the apex.
-		return Mathf.Sqrt(bounceFactor * 2 * jumpHeight * gravity);
+		return Mathf.Sqrt(2 * jumpHeight * gravity);
     }
 }
