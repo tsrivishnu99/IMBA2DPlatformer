7e853e6c628d602333b602af619da3f0d6513afc
unknown <lv1018@IGMGRAD-13.main.ad.rit.edu>
2015-09-25 17:59:05 -0400

    More Tweaks ! Much Better Jumping

--- Assets/Scripts/playerController_v2.cs
+++ Assets/Scripts/playerController_v2.cs
@@ -10,7 +10,6 @@ public class playerController_v2 : MonoBehaviour
     public float gravity = 10.0f;
     public float maxVelocityChange = 5.0f;
     public float boost ;
-    public bool canJump = true;
     public float jumpHeight;
     public float horizontalPush;
     public float rotationSpeed = 2.0f;
@@ -51,7 +50,7 @@ public class playerController_v2 : MonoBehaviour
             rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
 
             // Jump
-            if (canJump && Input.GetButtonDown("Jump"))
+            if (Input.GetButtonDown("Jump"))
             {
                 grounded = false;
                 rigidbody.velocity = new Vector3(velocity.x, CalculateJumpVerticalSpeed(), velocity.z);
@@ -87,6 +86,8 @@ public class playerController_v2 : MonoBehaviour
 
                 rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
             }
+            // We apply gravity manually for more tuning control
+            rigidbody.AddForce(new Vector3(0, -gravity * rigidbody.mass, 0));
         }
 
         if(isFlightMode)
@@ -94,9 +95,7 @@ public class playerController_v2 : MonoBehaviour
             rigidbody.velocity = new Vector3(0f, 2f, 0f);
         }
         
-        // We apply gravity manually for more tuning control
-        rigidbody.AddForce(new Vector3(0, -gravity * rigidbody.mass, 0));
-        grounded = false;
+
         if (this.transform.position.y >= groundPosis + 3.0f && !isFlightMode &&!isFlightPrev)
         {
 
@@ -106,20 +105,10 @@ public class playerController_v2 : MonoBehaviour
        
     }
 
-    void OnCollisionStay(Collision collisionInf)
-    {
-        if (collisionInf.collider.name.Contains("Platform"))
-        {
-            grounded = true;
-         
-        }
-
-    }
     void OnCollisionEnter(Collision collisionInf)
     {
 
         RaycastHit hit;
-
         Physics.Raycast(this.transform.position, new Vector3(Input.GetAxis("Horizontal"), 0f, 0f), out hit, 1f);
         Debug.DrawRay(this.transform.position, new Vector3(Input.GetAxis("Horizontal"), 0f, 0f), Color.blue);
 
@@ -127,14 +116,16 @@ public class playerController_v2 : MonoBehaviour
         //Debug.Log(angle + "" + hit.normal + "" + Input.GetAxis("Horizontal"));
 
 
-        if (angle == 180)
+       
+       if (collisionInf.collider.name.Contains("Platform"))
         {
-            Vector3 velocity = rigidbody.velocity;
-            rigidbody.velocity = new Vector3(CalculateJumpHorizontalSpeed(), velocity.y, velocity.z);
+            if (angle == 180)
+            {
+                Vector3 velocity = rigidbody.velocity;
+                rigidbody.velocity = new Vector3(CalculateJumpHorizontalSpeed(), velocity.y, velocity.z);
 
-        }
-        else if (collisionInf.collider.name.Contains("Platform"))
-        {
+            }
+
             grounded = true;
             isFlightPrev = false;
         }
