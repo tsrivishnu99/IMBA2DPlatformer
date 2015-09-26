9139299b009c96ff7f0375ae783bf1268db35d11
Mrinal Jain <mnj8396@rit.edu>
2015-09-26 17:09:57 -0400

    Added collision detection to spikes to that player dies when they hit the spikes.

---
+++ Assets/Scripts/SpikeObstacle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpikeObstacle : MonoBehaviour {
+    GameObject player;
+    Vector3 startPosition;
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        startPosition = player.transform.position;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            player.transform.position = startPosition;
+        }
+    }
+}
